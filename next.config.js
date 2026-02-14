d79873fb6c93231c40bafdcd57cb50447e678b4a
Khurram Jalil <khurram.jalil@gmail.com>
2026-02-14 19:03:16 +0500

    remove trailingSpace from next.config

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const nextConfig = {
     reactStrictMode: true,
     output: 'export',       // Required for GitHub Pages (Static Site Generation)
     basePath: '/movie-mania', // Change this to your actual repository name
-    trailingSlash: true,  // Ensure trailing slash for GitHub Pages
     images: {
         unoptimized: true,    // Required for 'output: export'
         domains: ['image.tmdb.org', 'www.fillmurray.com', 'www.themoviedb.org'],
