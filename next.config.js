a446b63848f5212fe2d6256d0eec4e28ed02871c
Khurram Jalil <khurram.jalil@gmail.com>
2026-04-18 11:50:48 +0500

    Revert "fix: vulernabilities"
    This reverts commit 7bd2ccdcc66f44cf43c94cbc2aeb03889dddf7d7.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    //output: 'export',       // Required for GitHub Pages (Static Site Generation)
+    output: 'export',       // Required for GitHub Pages (Static Site Generation)
     basePath: '/movie-mania', // Change this to your actual repository name
     images: {
         unoptimized: true, // Keep this for your static export
